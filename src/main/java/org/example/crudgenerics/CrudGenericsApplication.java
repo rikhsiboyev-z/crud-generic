42402a100d3a57eed78568dacb9b29f89997f867
egoist <rikhsiboyevzaxa@gmail.com>
2024-01-07 16:46:00 +0500

    crud - generic

---
+++ src/main/java/org/example/crudgenerics/CrudGenericsApplication.java
@@ -0,0 +1,13 @@
+package org.example.crudgenerics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudGenericsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudGenericsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/crudgenerics/CrudGenericsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crudgenerics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudGenericsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
